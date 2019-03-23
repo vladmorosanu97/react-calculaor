b0110aaaf6b70e044dc94d5114abd8a141a9044f
Vlad Morosanu <vladmorosanu97@gmail.com>
2019-03-23 10:57:34 +0200

    Updates

---
+++ precache-manifest.285d33e81c109e9b0ae267c59084edc6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e546b3e323215319a407",
+    "url": "/react-calculator/static/js/runtime~main.00b8a6fe.js"
+  },
+  {
+    "revision": "c7e5f6953de494509ba7",
+    "url": "/react-calculator/static/js/main.23cc066d.chunk.js"
+  },
+  {
+    "revision": "1ee21af30f30f8e8fce3",
+    "url": "/react-calculator/static/js/2.32c84693.chunk.js"
+  },
+  {
+    "revision": "c7e5f6953de494509ba7",
+    "url": "/react-calculator/static/css/main.76c38474.chunk.css"
+  },
+  {
+    "revision": "6b0cd7f1fa3a5930d4ac505cb8812f8f",
+    "url": "/react-calculator/index.html"
+  }
+];
